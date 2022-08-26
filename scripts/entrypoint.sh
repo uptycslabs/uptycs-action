442ab510ecb36ca2acd5cb1d938629e04815d9e6
Jonathan Nappi <jnappi@uptycs.com>
2022-08-26 14:32:34 -0400

    remove LD environment variable

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -81,7 +81,6 @@ debug "chroot-ing to ${CHROOT_DIR} to run ${BINARY_DIR}/osquery-scan"
 ls -alh ${BIN_DIR}
 
 exec chroot ${CHROOT_DIR} \
-    LD_LIBRARY_PATH=${LIB_DIR} \
     ${BINARY_DIR}/osquery-scan \
     --flagfile=${SOFTWARE_DIR}/etc/osquery.flags \
     --disable_events \
