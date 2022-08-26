f640109fa7252aca8e45923b32c4a2c4280abd07
Jonathan Nappi <jnappi@uptycs.com>
2022-08-26 10:06:23 -0400

    debugging

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -71,9 +71,8 @@ debug "ls /usr"
 ls -alh /usr
 
 mkdir "${CHROOT_DIR}/lib"
-mkdir "${CHROOT_DIR}/lib64"
 mount --bind /usr/lib "${CHROOT_DIR}/lib"
-mount --bind /usr/lib64 "${CHROOT_DIR}/lib64"
+mount --bind /usr/lib "${CHROOT_DIR}/lib64"
 
 debug "chroot-ing to ${CHROOT_DIR} to run ${BINARY_DIR}/osquery-scan"
 ls -alh ${BIN_DIR}
