a53d973b975341c8d184a6fb5cb06e2874fc7e92
Jonathan Nappi <jnappi@uptycs.com>
2022-08-30 09:42:44 -0400

    clean up explicit osquery flags

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -19,18 +19,10 @@ mkdir /var/log/osquery
     /usr/local/bin/osquery-scan \
     --flagfile=${INPUTS_DIR}/flags/osquery.flags \
     --disable_events \
-    --disable-database \
+    --disable-database  \
+    --shell_log_query_result=vulnerabilities \
     --verbose \
-    --sysfs_mountpoint=/sys \
-    --ebpf_program_location=${BINARY_DIR}/bpf_progs.o \
-    --enroll_secret_path=${INPUTS_DIR}/secrets/uptycs.secret \
-    --augeas_lenses=${SOFTWARE_DIR}/etc/lenses \
-    --ebpf_default_offsets=${SOFTWARE_DIR}/etc/ebpf_offsets.json \
-    --database_path=${SOFTWARE_DIR}/osquery.db \
-    --syslog_pipe_path=${SOFTWARE_DIR}/syslog_pipe \
-    --pidfile=${SOFTWARE_DIR}/var/run/osqueryd.pid \
     --config_tls_max_attempts=2 \
-    --read_max=300000000 \
-    --redirect_stderr=false \
+    --read_max=300000000
     --tls_dump \
     "SELECT *, (CASE WHEN cvss_score/1 >= ${FATAL_CVSS_SCORE} THEN 1 ELSE 0 END) AS fatal FROM vulnerabilities WHERE system_type = 'docker_image' AND system_id = '${IMAGE_ID}' AND verbose = 1" $@
