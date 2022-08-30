b2ba039a35a8576112df475f0dcc93bb3807f357
Jonathan Nappi <jnappi@uptycs.com>
2022-08-30 15:50:24 -0400

    add some additional options

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+set -ex
+
+err() {
+  echo "ERROR: $1"
+  exit 1
+}
+
+# Validate that required variables are set.
 if [ -z ${FATAL_CVSS_SCORE} ]; then
     err "FATAL_CVSS_SCORE MUST be set"
 fi
@@ -8,15 +16,10 @@ if [ -z ${IMAGE_ID} ]; then
     err "IMAGE_ID MUST be set"
 fi
 
-# If present, remove the leading sha256: prefix from the image id.
+# If present, remove the leading 'sha256:' prefix from the image id.
 IMAGE_ID=$(echo ${IMAGE_ID} | sed 's/.*://')
 
-INPUTS_DIR=/etc/osquery
-SOFTWARE_DIR=/opt/uptycs/osquery
-BINARY_DIR=${SOFTWARE_DIR}/bin
-
 mkdir /var/log/osquery
-
 /opt/uptycs/osquery/lib/ld-linux \
     --library-path /opt/uptycs/osquery/lib \
     /usr/local/bin/osquery-scan \
@@ -29,4 +32,5 @@ mkdir /var/log/osquery
     --read_max=300000000 \
     --redirect_stderr=false \
     --tls_dump \
+    --compliance_data_in_json=true \
     "SELECT *, (CASE WHEN cvss_score/1 >= ${FATAL_CVSS_SCORE} THEN 1 ELSE 0 END) AS fatal FROM vulnerabilities WHERE system_type = 'docker_image' AND system_id = '${IMAGE_ID}' AND verbose = 1" $@
