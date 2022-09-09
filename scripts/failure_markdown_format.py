42fb19bdaf6c5d88402b2d3444665321a3a8ec6b
Jonathan Nappi <jnappi@uptycs.com>
2022-09-09 15:42:29 -0400

    formatting updates

--- scripts/failure_markdown_format.py
+++ scripts/failure_markdown_format.py
@@ -1,13 +1,56 @@
 #!/usr/bin/env python3
 
-import json, sys
+import os
+import json
+import sys
+from typing import Tuple, List, Dict, Any
 
-newline = "\n"
-dnewline = f"{newline}{newline}"
-vulns = json.loads("".join(sys.stdin))
+#: The array of colums to render into the markdown table.
+COLUMNS = ('cve_list', 'package_name', 'package_version', 'hrefs')
 
-fmt_vuln_header = lambda vuln: f'# {vuln["cve_list"]} {vuln["package_name"]} {vuln["package_version"]}'
-fmt_vuln_hrefs = lambda vuln: newline.join(["<" + href + ">" for href in vuln["hrefs"].split(",") if href])
-fmt_vuln = lambda vuln: dnewline.join([fmt_vuln_header(vuln), vuln["description"], fmt_vuln_hrefs(vuln)])
+FATAL_CVSS_SCORE = 'FATAL_CVSS_SCORE'
 
-print(dnewline.join([fmt_vuln(vuln) for vuln in vulns]))
+
+def column_title(column: str) -> str:
+    """Render the specified column so that it can be used in the header column
+    of a markdown table.
+    """
+    return column.replace('_', ' ').title()
+
+
+def new_vulns_table(columns: Tuple[str], vulns: List[Dict[str, Any]]) -> str:
+    """Render the provided vulnerability data into a markdown table.
+    """
+    lines = [
+        '| ' + ' | '.join([column_title(column) for column in columns]) + ' |',
+        '| ' + ' | '.join(['-'*len(column) for column in columns]) + ' |'
+    ]
+
+    # Filter out only the vulnerabilities that were flagged as fatal.
+    fatal_vulns = [vuln for vuln in vulns if vuln['fatal'] == '1']
+
+    lines.extend([
+        f'| {vuln[column]} |' for vuln in fatal_vulns for column in columns
+    ])
+
+    return '\n'.join(lines)
+
+
+def render_markdown_report(columns: Tuple[str], vulns: List[Dict[str, Any]]) -> str:
+    """Render a markdown report detailing the fatal vulnerabilities detected.
+    """
+    header = '\n\n'.join([
+        '# Critical Vulnerabilities',
+        f'The following vulnerabilities exceeded the specified {FATAL_CVSS_SCORE} of {os.getenv(FATAL_CVSS_SCORE)}.'
+    ])
+
+    table = new_vulns_table(columns, vulns)
+
+    return '\n\n'.join([header, table])
+
+if __name__ == '__main__':
+    # Read the vulnerabilities JSON array piped over stdin.
+    vulns = json.loads("".join(sys.stdin))
+
+    # Render the fatal vulnerabilities from our results as a markdown report.
+    print(render_markdown_report(COLUMNS, vulns))
